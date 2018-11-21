f83cdefbfc9fc612d76c30a8511b6771afc5744f
kicyiu <albertokicyiu@gmail.com>
2018-11-21 08:15:06 -0300

    Primer Commit

---
+++ MyLocations/LocationDetailsViewController..swift
@@ -0,0 +1,150 @@
+//
+//  LocationDetailsViewController..swift
+//  MyLocations
+//
+//  Created by Alberto Tsang on 11/18/18.
+//  Copyright © 2018 kicyiusoft. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+private let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .short
+    print("Date formatter created")
+    return formatter
+}()
+
+class LocationDetailsViewController: UITableViewController {
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var placemark: CLPlacemark?
+    var categoryName = "No Category"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        descriptionTextView.text = ""
+        categoryLabel.text = categoryName
+        latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        if let placemark = placemark {
+            addressLabel.text = string(from: placemark)
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+        dateLabel.text = format(date: Date())
+        
+        //To add gesture recognizar in order to make keyborad disappears once the user tap outside the cursor
+        let gestureRecognizer = UITapGestureRecognizer(target: self,
+                                                       action: #selector(hideKeyboard))
+        gestureRecognizer.cancelsTouchesInView = false
+        tableView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func hideKeyboard(_ gestureRecognizer: UIGestureRecognizer) {
+        let point = gestureRecognizer.location(in: tableView)
+        let indexPath = tableView.indexPathForRow(at: point)
+        if indexPath != nil && indexPath!.section == 0
+            && indexPath!.row == 0 {
+            return
+        }
+        descriptionTextView.resignFirstResponder()
+    }
+    
+    func string(from placemark: CLPlacemark) -> String {
+        var text = ""
+        if let s = placemark.subThoroughfare {
+            text += s + " "
+        }
+        if let s = placemark.thoroughfare {
+            text += s + ", "
+        }
+        if let s = placemark.locality {
+            text += s + ", "
+        }
+        if let s = placemark.administrativeArea {
+            text += s + " "
+        }
+        if let s = placemark.postalCode {
+            text += s + ", "
+        }
+        if let s = placemark.country {
+            text += s
+        }
+        return text
+    }
+    
+    func format(date: Date) -> String {
+        return dateFormatter.string(from: date)
+    }
+    
+    
+    @IBAction func done() {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - UITableViewDelegate
+    //This delegate method is called by the table view when it loads its cells
+    override func tableView(_ tableView: UITableView,
+                            heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 88
+        } else if indexPath.section == 2 && indexPath.row == 2 {
+            //CGRect is a struct that describes a rectangle. This rectangle has an origin made upof a CGPoint value with an (X, Y) coordinate, and a CGSize value for the width and height
+            addressLabel.frame.size = CGSize(
+                width: view.bounds.size.width - 115,
+                height: 10000)
+            addressLabel.sizeToFit()
+            addressLabel.frame.origin.x = view.bounds.size.width -
+                addressLabel.frame.size.width - 15
+            return addressLabel.frame.size.height + 20
+        } else {
+            return 44
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PickCategory" {
+            let controller = segue.destination as! CategoryPickerViewController
+            controller.selectedCategoryName = categoryName
+        }
+    }
+    
+    @IBAction func categoryPickerDidPickCategory(_ segue: UIStoryboardSegue)
+    {
+        let controller = segue.source as! CategoryPickerViewController
+        categoryName = controller.selectedCategoryName
+        print("Lication details selectedCategoryName: \(categoryName)")
+        categoryLabel.text = categoryName
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if indexPath.section == 0 || indexPath.section == 1 {
+            print("indexPath: \(indexPath)" )
+            return indexPath
+        } else {
+            print("Null tap")
+            return nil
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            //Make keyboard appears no matter where did you tap on the cell
+            descriptionTextView.becomeFirstResponder()
+        }
+    }
+}
